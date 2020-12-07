cbdaff7e977d626f982c05a97167dcca3a36f305
ryuch91 <ryuch91@gmail.com>
2020-12-08 00:35:40 +0900

    GraphQL Node Type중 frontmatter.tags를 nullable하게 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -137,7 +137,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
-      tags: [String!]!
+      tags: [String!]
     }
 
     type Fields {
